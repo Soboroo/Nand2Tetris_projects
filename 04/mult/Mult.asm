8704a0801f8e8f6a49e7eedcdd21dd345e6f7cc0
Yeongyun Woo <woo036325@gmail.com>
2023-08-28 13:08:12 +0000

    Project 04 Mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,28 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@R2
+M=0
+@R0
+D=M
+@R3
+M=D
+(LOOP)
+	// D = condition
+	@R1
+	D=M
+	@LOOP_END
+	D;JEQ
+	
+	@R3
+	D=M
+	@R2
+	M=M+D
+	@R1
+	M=M-1
+
+	@LOOP
+	0;JMP
+(LOOP_END)
+@LOOP_END
+0;JMP
